94fe2f71221aa388f58972d186b997796d141c1b
efjay <efjaymania@gmail.com>
2022-02-28 17:48:26 +0100

    added final edits

---
+++ components/Personal/InkChallenge.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data(){
+        return{
+            url: "/pics/personal/ink_challenge/"
+        }
+    },
+    computed: {
+    }
+}
+</script>
+
+<template>
+<div class="display__controller">
+    <div class="previewCard" v-for="index in 20" :key="index" :style="{backgroundImage: 'url('+url+index+'.webp'+')'}">
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
