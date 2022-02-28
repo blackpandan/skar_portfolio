c8aa932dfdfc7702a451cfc9acf139056d60c931
efjay <efjaymania@gmail.com>
2022-02-28 18:21:24 +0100

    increased seo

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    head(){
+        return{
+            title: "About Me - Skar",
+           
+        }
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
