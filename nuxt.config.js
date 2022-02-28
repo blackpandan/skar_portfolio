7d5a92716a4a8f7d6910033b3ed67cdb275842f4
efjay <efjaymania@gmail.com>
2022-02-28 18:47:22 +0100

    removed sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@blokwise/dynamic',
     '@nuxtjs/axios',
-    '@nuxtjs/sitemap'
+    // '@nuxtjs/sitemap'
   ],
 
 
@@ -90,5 +90,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
 }
